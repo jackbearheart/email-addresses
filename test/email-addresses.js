79d7c0d2490ed722fe8f2dd407c38cd452f93cf4
Matt Sergeant <matt@sergeant.org>
2017-02-22 19:02:02 -0500

    Strip whitespace from domains

--- test/email-addresses.js
+++ test/email-addresses.js
@@ -270,3 +270,14 @@ test("dots in unquoted display-names", function (t) {
 
     t.end();
 });
+
+test("whitespace in domain", function (t) {
+    var fxn, result;
+    fxn = addrs.parseOneAddress;
+
+    result = fxn('":sysmail"@ Some-Group. Some-Org');
+    t.ok(result, "spaces in domain parses ok");
+    t.equal(result.domain, 'Some-Group.Some-Org', "domain parsing strips whitespace");
+    
+    t.end();
+})
