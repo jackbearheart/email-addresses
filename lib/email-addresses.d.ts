2ff852e895f46b2b9fb89408e60a44f1b8d64e68
Wilson Hobbs <wilsonhobbs1@gmail.com>
2017-06-10 21:33:03 -0400

    Remove external module format as per #19

--- lib/email-addresses.d.ts
+++ lib/email-addresses.d.ts
@@ -1,28 +1,26 @@
-declare module "email-addresses" {
-    export function parseOneAddress(opts: string): ParsedEmailOutput;
-    export function parseAddressList(opts: string): ParsedEmailOutput[];
-    export interface ParsedEmailOutput { 
-        parts: { 
-            name: {
-                tokens: string;
-                semantic: string;
-            };
-            address: {
-                tokens: string;
-                semantic:string;
-            };
-            local: {
-                tokens: string;
-                semantic:string;
-            };
-            domain: {
-                tokens: string;
-                semantic:string;
-            };
+export function parseOneAddress(opts: string): ParsedEmailOutput;
+export function parseAddressList(opts: string): ParsedEmailOutput[];
+export interface ParsedEmailOutput { 
+    parts: { 
+        name: {
+            tokens: string;
+            semantic: string;
         };
-        name: string;
-        address: string;
-        local: string;
-        domain: string;
-    }
+        address: {
+            tokens: string;
+            semantic:string;
+        };
+        local: {
+            tokens: string;
+            semantic:string;
+        };
+        domain: {
+            tokens: string;
+            semantic:string;
+        };
+    };
+    name: string;
+    address: string;
+    local: string;
+    domain: string;
 }
