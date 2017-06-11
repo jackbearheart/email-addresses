09da79b2f13968004cad5da0a5eb9b34e04ffb89
Wilson Hobbs <wilsonhobbs1@gmail.com>
2017-06-10 21:31:33 -0400

    Ammend typings for better specificity.

--- lib/email-addresses.d.ts
+++ lib/email-addresses.d.ts
@@ -1,4 +1,28 @@
 declare module "email-addresses" {
-    export function parseOneAddress(opts: any): any;
-    export function parseAddressList(opts: any): any;
+    export function parseOneAddress(opts: string): ParsedEmailOutput;
+    export function parseAddressList(opts: string): ParsedEmailOutput[];
+    export interface ParsedEmailOutput { 
+        parts: { 
+            name: {
+                tokens: string;
+                semantic: string;
+            };
+            address: {
+                tokens: string;
+                semantic:string;
+            };
+            local: {
+                tokens: string;
+                semantic:string;
+            };
+            domain: {
+                tokens: string;
+                semantic:string;
+            };
+        };
+        name: string;
+        address: string;
+        local: string;
+        domain: string;
+    }
 }
