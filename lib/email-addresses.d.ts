3053f8a76075e4fe926859f6b259f207980f657a
Wilson Hobbs <wilsonhobbs1@gmail.com>
2017-06-15 16:28:10 -0400

    add module back

--- lib/email-addresses.d.ts
+++ lib/email-addresses.d.ts
@@ -1,26 +1,28 @@
-export function parseOneAddress(opts: string): ParsedEmailOutput;
-export function parseAddressList(opts: string): ParsedEmailOutput[];
-export interface ParsedEmailOutput { 
-    parts: { 
-        name: {
-            tokens: string;
-            semantic: string;
+declare module "email-addresses" {
+    export function parseOneAddress(opts: string): ParsedEmailOutput;
+    export function parseAddressList(opts: string): ParsedEmailOutput[];
+    export interface ParsedEmailOutput { 
+        parts: { 
+            name: {
+                tokens: string;
+                semantic: string;
+            };
+            address: {
+                tokens: string;
+                semantic:string;
+            };
+            local: {
+                tokens: string;
+                semantic:string;
+            };
+            domain: {
+                tokens: string;
+                semantic:string;
+            };
         };
-        address: {
-            tokens: string;
-            semantic:string;
-        };
-        local: {
-            tokens: string;
-            semantic:string;
-        };
-        domain: {
-            tokens: string;
-            semantic:string;
-        };
-    };
-    name: string;
-    address: string;
-    local: string;
-    domain: string;
+        name: string;
+        address: string;
+        local: string;
+        domain: string;
+    }
 }
