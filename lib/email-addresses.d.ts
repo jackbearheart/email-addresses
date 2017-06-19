2e7b5a2875b2b337bd8daa82aa8b8a8f001e0f6e
Jack Bearheart <johnrbowman@fastmail.fm>
2017-06-19 16:29:00 -0700

    add complete typescript typings

--- lib/email-addresses.d.ts
+++ lib/email-addresses.d.ts
@@ -1,28 +1,44 @@
-declare module "email-addresses" {
-    export function parseOneAddress(opts: string): ParsedEmailOutput;
-    export function parseAddressList(opts: string): ParsedEmailOutput[];
-    export interface ParsedEmailOutput { 
-        parts: { 
-            name: {
-                tokens: string;
-                semantic: string;
-            };
-            address: {
-                tokens: string;
-                semantic:string;
-            };
-            local: {
-                tokens: string;
-                semantic:string;
-            };
-            domain: {
-                tokens: string;
-                semantic:string;
-            };
+declare module emailAddresses {
+    function parseOneAddress(input: string | Options): ParsedAddress;
+    function parseAddressList(input: string | Options): ParsedAddress[];
+    interface ParsedAddress {
+        parts: {
+            name: ASTNode;
+            address: ASTNode;
+            local: ASTNode;
+            domain: ASTNode;
+            comments: ASTNode[];
         };
         name: string;
         address: string;
         local: string;
         domain: string;
     }
+
+    interface ASTNode {
+        name: string;
+        tokens: string;
+        semantic: string;
+        children: ASTNode[];
+    }
+
+    interface Options {
+        input: string;
+        partial?: boolean;
+        rejectTLD?: boolean;
+        rfc6532?: boolean;
+        simple?: boolean;
+        strict?: boolean;
+    }
+
+    interface ParsedResult {
+        ast: ASTNode;
+        addresses: ParsedAddress[];
+    }
+}
+
+declare function emailAddresses(opts: emailAddresses.Options): emailAddresses.ParsedResult;
+
+declare module "email-addresses" {
+    export = emailAddresses;
 }
