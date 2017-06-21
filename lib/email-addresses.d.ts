a12b003282fa15a565e087a49be260f5670bc58a
Jack Bearheart <johnrbowman@fastmail.fm>
2017-06-20 17:44:37 -0700

    update ts definitions with new options and group type

--- lib/email-addresses.d.ts
+++ lib/email-addresses.d.ts
@@ -1,7 +1,12 @@
 declare module emailAddresses {
-    function parseOneAddress(input: string | Options): ParsedAddress;
-    function parseAddressList(input: string | Options): ParsedAddress[];
-    interface ParsedAddress {
+    function parseOneAddress(input: string | Options): ParsedMailbox | ParsedGroup;
+    function parseAddressList(input: string | Options): (ParsedMailbox | ParsedGroup)[];
+    function parseFrom(input: string | Options): (ParsedMailbox | ParsedGroup)[];
+    function parseSender(input: string | Options): ParsedMailbox | ParsedGroup;
+    function parseReplyTo(input: string | Options): (ParsedMailbox | ParsedGroup)[];
+
+    interface ParsedMailbox {
+        node?: ASTNode;
         parts: {
             name: ASTNode;
             address: ASTNode;
@@ -9,10 +14,21 @@ declare module emailAddresses {
             domain: ASTNode;
             comments: ASTNode[];
         };
+        type: string;
         name: string;
         address: string;
         local: string;
         domain: string;
+    }
+
+    interface ParsedGroup {
+        node?: ASTNode;
+        parts: {
+            name: ASTNode;
+        };
+        type: string;
+        name: string;
+        addresses: ParsedMailbox[];
     }
 
     interface ASTNode {
@@ -24,16 +40,18 @@ declare module emailAddresses {
 
     interface Options {
         input: string;
+        oneResult?: boolean;
         partial?: boolean;
         rejectTLD?: boolean;
         rfc6532?: boolean;
         simple?: boolean;
+        startAt?: string;
         strict?: boolean;
     }
 
     interface ParsedResult {
         ast: ASTNode;
-        addresses: ParsedAddress[];
+        addresses: (ParsedMailbox | ParsedGroup)[];
     }
 }
 
@@ -42,3 +60,48 @@ declare function emailAddresses(opts: emailAddresses.Options): emailAddresses.Pa
 declare module "email-addresses" {
     export = emailAddresses;
 }
+
+/* Example usage:
+
+// Run this file with:
+//  tsc test.ts && NODE_PATH="../emailaddresses/lib" node test.js
+/// <reference path="../emailaddresses/lib/email-addresses.d.ts"/>
+import emailAddresses = require('email-addresses');
+
+function isParsedMailbox(mailboxOrGroup: emailAddresses.ParsedMailbox | emailAddresses.ParsedGroup): mailboxOrGroup is emailAddresses.ParsedMailbox {
+    return mailboxOrGroup.type === 'mailbox';
+}
+
+var testEmail : string = "TestName (a comment) <test@example.com>";
+console.log(testEmail);
+
+var parsed = emailAddresses.parseOneAddress(testEmail);
+console.log(parsed);
+
+var a : string = parsed.parts.name.children[0].name;
+console.log(a);
+
+if (isParsedMailbox(parsed)) {
+    var comment : string = parsed.parts.comments[0].tokens;
+    console.log(comment);
+} else {
+    console.error('error, should be a ParsedMailbox');
+}
+
+//
+
+var emailList : string = "TestName <test@example.com>, TestName2 <test2@example.com>";
+console.log(emailList);
+
+var parsedList = emailAddresses.parseAddressList(emailList);
+console.log(parsedList);
+
+var b : string = parsedList[1].parts.name.children[0].semantic;
+console.log(b);
+
+//
+
+var parsedByModuleFxn = emailAddresses({ input: emailList, rfc6532: true });
+console.log(parsedByModuleFxn.addresses[0].name);
+
+*/
